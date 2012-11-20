2be78d4b0d24c1044a08b99b9421235f232b3264
jbbarth <jeanbaptiste.barth@gmail.com>
2012-11-20 13:24:20 +0100

    Add rspec + guard for testing

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'rspec'
+require 'cartoque_client'
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+end
