6102a092d1e5ed94980955fbb91d3a51171230d3
jbbarth <jeanbaptiste.barth@gmail.com>
2012-11-21 13:22:15 +0100

    Ensure that we don't screw up environment variables with our tests

--- spec/cartoquist/client_spec.rb
+++ spec/cartoquist/client_spec.rb
@@ -3,6 +3,18 @@ require "cartoquist/client"
 
 describe Cartoquist::Client do
   describe "#new" do
+    around(:each) do |example|
+      url, token = ENV["CARTOQUIST_URL"], ENV["CARTOQUIST_TOKEN"]
+      ENV["CARTOQUIST_URL"] = ENV["CARTOQUIST_TOKEN"] = nil
+      begin
+        example.run
+      ensure
+        #restore parameters
+        ENV["CARTOQUIST_URL"] = url
+        ENV["CARTOQUIST_TOKEN"] = token
+      end
+    end
+
     it "raises if no parameter" do
       expect do
         Cartoquist::Client.new
@@ -16,19 +28,12 @@ describe Cartoquist::Client do
     end
 
     it "accepts arguments through ENV parameters" do
-      begin
-        old_env = ENV.clone
-        #useful for command-line, testing, etc.
-        ENV["CARTOQUIST_URL"] = "cartoque.test"
-        ENV["CARTOQUIST_TOKEN"] = "token-from-env"
-        api = Cartoquist::Client.new
-        api.url.should == "cartoque.test"
-        api.token.should == "token-from-env"
-      ensure
-        #restore parameters
-        ENV["CARTOQUIST_URL"] = old_env["CARTOQUIST_URL"]
-        ENV["CARTOQUIST_TOKEN"] = old_env["CARTOQUIST_TOKEN"]
-      end
+      #useful for command-line, testing, etc.
+      ENV["CARTOQUIST_URL"] = "cartoque.test"
+      ENV["CARTOQUIST_TOKEN"] = "token-from-env"
+      api = Cartoquist::Client.new
+      api.url.should == "cartoque.test"
+      api.token.should == "token-from-env"
     end
   end
 end
